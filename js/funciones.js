d7b7ce5d6f70f005e8c5d1c25949f0e3049ccc66
Cristian Salas Ventura <240186@utxicotepec.edu.mx>
2026-01-08 14:28:57 -0600

    Crear funciones js y estilo del encabezado]

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,32 @@
 function saludar(){
     alert("hola broder Bienvenido a T.I");
 }
+
+function suma(){
+    var valor1 = 24;
+    var valor2 = 13;
+    var resultado = valor1 + valor2;
+    alert("Resultado de la suma de es: "+resultado);
+}
+
+function resta(){
+    var valor1 = 39;
+    var valor2 = 16;
+     var resultado = valor1 - valor2;
+    alert("Resultado de la resta es: "+resultado);
+}
+
+function multi(){
+    var valor1 = 11;
+    var valor2 = 4;
+     var resultado = valor1 * valor2;
+    alert("Resultado de la multiplicacion es: "+resultado);
+}
+
+function dividir(){
+    var valor1 = 39;
+    var valor2 = 4;
+     var resultado = valor1 / valor2;
+    alert("Resultado de la divicion es: "+resultado);
+}
+
