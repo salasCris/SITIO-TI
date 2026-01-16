4f5644a6c3a4335346adc29d61dadb64d81bbaa5
Cristian Salas Ventura <240186@utxicotepec.edu.mx>
2026-01-16 14:12:59 -0600

    add :se agregaron funciones java Script

--- js/funciones.js
+++ js/funciones.js
@@ -30,3 +30,42 @@ function dividir(){
     alert("Resultado de la divicion es: "+resultado);
 }
 
+function ejercicio2(){
+    let a, b;
+    a = 10; b =5;
+    resultado =a+b;
+
+    alert(`La suma de a + b es ${resultado}`);
+}
+
+function ejercicio3(){
+    let num1=0;
+    let num2=0;
+
+    num1 = num1 +1;
+    num2 = num2 +5;
+
+    alert(`el primer numero ahora es  ${num1}`);
+    alert(`La segundo numero ahora es ${num2}`);
+}
+
+function ejercicio4(){
+    const a = "HOLAA";
+    let b = "MUNDOOOOO!";
+
+    document.write("Constante a contiene " + a );
+    document.write("</br>");
+    document.write("Variable b contiene " + b );
+    document.write("</br>");
+    document.write(a + b);
+}
+
+function ejercicio5(){
+    let a, b;
+    a =2; b=8;
+    resultado= a *b;
+    document.write(`Variable a contiene ${a} <br>`);
+    document.write(`Variable b contiene ${b} <br>`);
+    document.write(`El producto de a por b es ${resultado}`);
+
+}
